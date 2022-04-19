7d0a155f6f73e7172a51a6605d805da0d36131c1
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2022-04-19 18:34:59 +0430

    Add primary button

--- react-quiz-app/src/components/logout-button.jsx
+++ react-quiz-app/src/components/logout-button.jsx
@@ -1,15 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { PrimaryButton } from "./primary-button";
 
 function LogoutButton() {
   const { logout } = useAuth0();
 
   return (
-    <button
-      onClick={() => logout({ returnTo: window.location.origin })}
-      className=" bg-blue-500 hover:bg-blue-600 text-white inline-flex items-center border-0 py-1 px-3 focus:outline-none  rounded mt-4 md:mt-0"
-    >
+    <PrimaryButton onClick={() => logout({ returnTo: window.location.origin })}>
       Logout
-    </button>
+    </PrimaryButton>
   );
 }
 
--- react-quiz-app/src/components/start-button.jsx
+++
@@ -1,9 +0,0 @@
-function StartButton() {
-  return (
-    <button className=" bg-blue-500 hover:bg-blue-600 text-white inline-flex items-center border-0 py-1 px-3 focus:outline-none  rounded mt-4 md:mt-0">
-      Start Quiz
-    </button>
-  );
-}
-
-export { StartButton };
